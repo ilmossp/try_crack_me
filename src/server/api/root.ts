ad090f46b6fc2a08823462f5bd855970029750f6
ilmossp <ilyamodeni@gmail.com>
2023-02-13 21:00:02 +0100

    fix: trpc types error

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,14 +1,10 @@
 import { createTRPCRouter } from "./trpc";
 import { exampleRouter } from "./routers/example";
 
-/**
- * This is the primary router for your server.
- *
- * All routers added in /api/routers should be manually added here
- */
-export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+
+export const Router = createTRPCRouter({
+  router: exampleRouter,
 });
 
 // export type definition of API
-export type AppRouter = typeof appRouter;
+export type AppRouter = typeof Router;
