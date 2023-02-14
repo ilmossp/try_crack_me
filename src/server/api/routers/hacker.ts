2a8bd6ae661fad9234478823d21f5365b1a69c81
ilmossp <ilyamodeni@gmail.com>
2023-02-14 22:19:54 +0100

    feat: outlined difficulty params

--- src/server/api/routers/hacker.ts
+++ src/server/api/routers/hacker.ts
@@ -5,7 +5,15 @@ export const hackerRouter = createTRPCRouter({
   challenge: publicProcedure
     .input(
       z.object({
-        difficulty: z.enum(["easy", "medium", "hard"]),
+        difficulty: z.object({
+            length: z.union([z.literal(6),z.literal(8),z.literal(12),z.literal(16)]),
+            specialChars: z.boolean(),
+            upperCase: z.boolean(),
+            numbers: z.boolean(),
+            hashingMethod: z.enum(["Argon2","bcrypt","scrypt"]),
+            salt: z.boolean(),
+            saltRounds: z.optional(z.number().int().max(10).min(0))
+        }),
       })
     )
     .query(({ input }) => {
