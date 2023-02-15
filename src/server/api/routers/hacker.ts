f5d2a7c9225111816dd7e4e79c2f02b1a76df69c
ilmossp <ilyamodeni@gmail.com>
2023-02-15 11:10:08 +0100

    started password generation

--- src/server/api/routers/hacker.ts
+++ src/server/api/routers/hacker.ts
@@ -1,22 +1,28 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
+const difficultySchema = z.object({
+  length: z.union([z.literal(6), z.literal(8), z.literal(12), z.literal(16)]),
+  specialChars: z.boolean(),
+  upperCase: z.boolean(),
+  numbers: z.boolean(),
+  hashingMethod: z.enum(["Argon2", "bcrypt", "scrypt"]),
+  salt: z.boolean(),
+  saltRounds: z.optional(z.number().int().max(10).min(0)),
+});
+
 export const hackerRouter = createTRPCRouter({
   challenge: publicProcedure
     .input(
       z.object({
-        difficulty: z.object({
-            length: z.union([z.literal(6),z.literal(8),z.literal(12),z.literal(16)]),
-            specialChars: z.boolean(),
-            upperCase: z.boolean(),
-            numbers: z.boolean(),
-            hashingMethod: z.enum(["Argon2","bcrypt","scrypt"]),
-            salt: z.boolean(),
-            saltRounds: z.optional(z.number().int().max(10).min(0))
-        }),
+        difficulty: difficultySchema,
       })
     )
     .query(({ input }) => {
       return { message: `so you have chosen ${input.difficulty}` };
     }),
 });
+
+function generatePassword(params: z.infer<typeof difficultySchema>) {
+    
+}
