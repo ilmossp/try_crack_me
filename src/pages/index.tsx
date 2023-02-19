83d2f6625da1721aacf19d8a090e4e5040e5dfc1
ilmossp <ilyamodeni@gmail.com>
2023-02-19 14:54:22 +0100

    fix: removed unnecessary useState

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { useState } from "react";
 import Header from "../components/Header";
 import { api } from "../utils/api";
 
