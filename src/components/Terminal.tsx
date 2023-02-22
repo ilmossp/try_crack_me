7af6a1fea0ad197120510668eabe24a19a334786
ilmossp <ilyamodeni@gmail.com>
2023-02-22 20:19:00 +0100

    feat: added error message to terminal

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -12,11 +12,12 @@ export function Terminal({ challenge }: TerminalProps) {
 
   const loading = "awaiting challenge ...";
   const success = "try crack this ";
-
+  const error = "an error occured while fetching, Try again "
+  
   return (
     <div className=" w-96 break-words rounded-md bg-black p-3 text-green-500">
       <span className="">{"> "}</span>
-      {isRefetching ? loading : isSuccess ? success + data : "click the green to start a challenge !!! "}
+      {isRefetching ? loading : isSuccess ? success + data : isError ? error :"click the green button to start a challenge !!! "}
     </div>
   );
 }
